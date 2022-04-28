5c46f0440da9da11825477ea1a5ae049997cf4e9
Ronit Khadgi <ihahsronit@gmail.com>
2022-04-28 17:06:35 +0545

    Summer project 2022

---
+++ frontend/src/components/MessageBox.js
@@ -0,0 +1,10 @@
+import Alert from 'react-bootstrap/Alert';
+
+
+export default function MessageBox(props){
+    return(
+    <Alert  variant={props.variant ||'info'}>{props.children}</Alert>
+
+  
+    )
+}
